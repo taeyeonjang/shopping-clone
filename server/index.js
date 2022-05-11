a8f44fb19c0f6950667cc4b17e91b5a00f510f52
tyJang <wxd8339@gmail.com>
2022-05-11 16:29:02 +0900

    1

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname +"../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
